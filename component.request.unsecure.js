f831b7c0eec8c850c6d1b5b5b93e6fb05ee8b3be
marchuanv <marchuanv@gmail.com>
2021-02-23 01:30:37 +0200

    refactor

---
+++ component.request.unsecure.js
@@ -0,0 +1,10 @@
+const logging = require("logging");
+const requestDeferred = require("component.request.deferred");
+logging.config.add("Sending Unsecure Request");
+module.exports = { 
+    send: async ({ host, port, path, method, headers, data }) => {
+        const requestUrl = `${host}:${port}${path}`;
+        logging.write("Sending Unsecure Request",`sending unsecured request to ${requestUrl}`);
+        return await requestDeferred.send({ host, port, path, method, headers, data });
+    }
+};
--- test.js
+++ test.js
@@ -1,36 +1,22 @@
-const requestSecure = require("./component.request.secure.js");
+const unsecureRequest = require("./component.request.unsecure.js");
 (async()=>{
-
-    await requestSecure.send({
-        host: "localhost", 
-        port: 5000, 
-        path: "/test", 
-        method:"POST", 
-        headers: { 
-            "Content-Type":"text/plain", 
-            username: "admin1", 
-            passphrase: "secure1",
+    const results = await unsecureRequest.send({
+        host: "localhost",
+        port: 5000,
+        path: "/test",
+        method:"POST",
+        headers: {
+            "Content-Type":"text/plain",
+            username: "admin1",
             fromhost: "localhost",
             fromport: 6000
-        }, 
-        data: "Hello World From Client" 
+        },
+        data: "Hello World From Client"
     });
+    if (results.statusCode !== 500){
+        throw new Error("Test Failed");
+    }
 
-    await requestSecure.send({
-        host: "localhost", 
-        port: 5000, 
-        path: "/test", 
-        method:"POST", 
-        headers: { 
-            "Content-Type":"text/plain", 
-            username: "admin1", 
-            passphrase: "secure1",
-            fromhost: "localhost",
-            fromport: 6000
-        }, 
-        data: "Hello World From Client" 
-    });
-    
 })().catch((err)=>{
     console.log(err);
 });
