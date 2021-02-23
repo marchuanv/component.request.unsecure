11331862ae9a1dc9fa935b74f2f774705ee42d1a
marchuanv <marchuanv@gmail.com>
2021-02-24 00:14:31 +0200

    fix

--- component.request.unsecure.js
+++ component.request.unsecure.js
@@ -5,6 +5,6 @@ module.exports = {
     send: async ({ host, port, path, method, username, fromhost, fromport, data }) => {
         const requestUrl = `${host}:${port}${path}`;
         logging.write("Sending Unsecure Request",`sending unsecured request to ${requestUrl}`);
-        return await requestDeferred.send({ host, port, path, method, headers: { username, fromhost, fromport }, data });
+        return await requestDeferred.send({ host, port, path, method, headers: { username: username || "", fromhost: fromhost || "", fromport: fromport || "" }, data });
     }
 };
