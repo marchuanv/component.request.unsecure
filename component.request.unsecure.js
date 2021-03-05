91245c95b4532dd0ba7127918d164d0b28ea6297
marchuanv <marchuanv@gmail.com>
2021-03-05 15:07:01 +0200

    refactor

--- component.request.unsecure.js
+++ component.request.unsecure.js
@@ -1,6 +1,6 @@
 const logging = require("logging");
 const requestDeferred = require("component.request.deferred");
-logging.config.add("Sending Unsecure Request");
+
 module.exports = { 
     send: async ({ host, port, path, method, username, fromhost, fromport, data }) => {
         const requestUrl = `${host}:${port}${path}`;
